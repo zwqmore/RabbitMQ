5ff5ae5b804c348372f3307606de8bba78ef25d9
zwqmore <2672879993@qq.com>
2021-11-16 16:54:15 +0800

    first pul

---
+++ src/test/java/com/zwq/rabbit/RabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwq.rabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
